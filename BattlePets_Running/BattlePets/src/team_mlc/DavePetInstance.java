b14116b504697435c9d8a3bf5750664ac5b097ea
Lukas J Peterson <petersonlu@uwplatt.edu>
2019-02-26 17:57:39 -0600

    Getting DavePetInstance set up, preparing for message passing

--- BattlePets_Running/BattlePets/src/team_mlc/DavePetInstance.java
+++ BattlePets_Running/BattlePets/src/team_mlc/DavePetInstance.java
@@ -1,13 +1,30 @@
 package team_mlc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
+import java.util.Random;
 
+import event.AttackEvent;
+import event.FightStartEvent;
+import event.RoundStartEvent;
 import player.PetInstance;
+import player.PetTypes;
 import python.PythonCommunicator;
 import settings.PlayerInfo;
 import skill.Skills;
 
 public class DavePetInstance extends PetInstance {
+	protected Map<Skills, Integer> rechargingOpponentSkills, jarvisRechargingSkills;
+	private int opponentIndex, jarvisIndex;
+	private PetTypes opponentType;
+	private Map<Integer, Double> randomDifference;
+	private double opponentHealth;
+	private Random random;
+	private int roundsSinceUse;
+	private double maxROFDiff;
+	private Skills predictedSkill;//Override from the Player... We want to use this one. 
+	
 	
 	private PythonCommunicator pyCom;
 	
@@ -15,6 +32,16 @@ public class DavePetInstance extends PetInstance {
 	{
 		super(playerInfo);
 		
+		this.randomDifference = new HashMap<Integer, Double>();
+		this.rechargingOpponentSkills = new HashMap<Skills, Integer>();
+		this.rechargingOpponentSkills.put(Skills.ROCK_THROW, 0);
+		this.rechargingOpponentSkills.put(Skills.SCISSORS_POKE, 0);
+		this.rechargingOpponentSkills.put(Skills.PAPER_CUT, 0);
+		this.rechargingOpponentSkills.put(Skills.SHOOT_THE_MOON, 0);
+		this.rechargingOpponentSkills.put(Skills.REVERSAL_OF_FORTUNE, 0);
+		this.random  = new Random(10320l);
+		roundsSinceUse = 0;
+		maxROFDiff = (-1) * Double.MAX_VALUE;
 	}
 
 	@Override
@@ -24,8 +51,22 @@ public class DavePetInstance extends PetInstance {
 	}
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
-		
-		
+	public void update(Observable arg0, Object event) 
+	{
+		if(event instanceof FightStartEvent)
+		{
+			FightStartEvent fse = (FightStartEvent) event;
+			rememberFightStartEvent(fse);
+		}
+		if(event instanceof RoundStartEvent)
+		{
+			RoundStartEvent rse = (RoundStartEvent) event;
+			rememberRoundStartEvent(rse);
+		}         
+		if(event instanceof AttackEvent)
+		{
+			AttackEvent ae = (AttackEvent) event;
+			this.rememberAttackEvent(ae);
+		}
 	}
 }

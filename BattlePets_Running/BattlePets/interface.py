dc6438ee5c68230e814a9cb36beaa933e3e97ee9
garrettluskey <garrett.luskey@gmail.com>
2019-03-26 17:45:00 -0500

    Implemented state class
    Changed interface dictionary to use the gamestate class and partially finished the state class. State still needs round information and update functions.

--- BattlePets_Running/BattlePets/interface.py
+++ BattlePets_Running/BattlePets/interface.py
@@ -1,42 +1,34 @@
 from pexpect import popen_spawn
 from pexpect import exceptions
 import os
+from gamestate import *
 
-player1_name = 'P1'
-player2_name = 'P2'
-
-
-pet1_name = 'PET1'
-pet2_name = 'PET2'
-
-starting_hp = 100.0
 
 # responses based on stdout value
-responses = {'Enter a random seed':'1', 
-                'Enter a number of players':'2', 
-                'Enter a number of fights':'1',
-                'Enter the player type for player 1':'2',
-                'Enter the pet type for player 1':'1',
-                'Enter a name for player 1':player1_name,
-                '{}: Enter a name for your pet'.format(player1_name):pet1_name, # if name changes change this line
+responses = {'Enter a random seed':gamestate.seed, 
+                'Enter a number of players':gamestate.num_players, 
+                'Enter a number of fights':gamestate.num_fights,
+                'Enter the player type for player 1':gamestate.players[0].player_type,
+                'Enter the pet type for player 1':gamestate.players[0].pet_type,
+                'Enter a name for player 1':gamestate.players[0].name,
+                '{}: Enter a name for your pet'.format(gamestate.players[0].name):gamestate.players[0].pet_name, # if name changes change this line
                 'Enter the player type for player 2':'2',
                 'Enter the pet type for player 2':'1',
-                'Enter a name for player 2':player2_name,
-                '{}: Enter a name for your pet'.format(player2_name):pet2_name, # if name changes change this line
-                'Enter a starting hp':starting_hp,
+                'Enter a name for player 2':gamestate.players[1].name,
+                '{}: Enter a name for your pet'.format(gamestate.players[1].name):gamestate.players[1].pet_name, # if name changes change this line
+                'Enter a starting hp':gamestate.players[0].starting_hp,
                 'Round 1 Started':None,
                 'Battle Started':None,
                 'Number of Fights = ':None,
-                'Starting HP: {}'.format(starting_hp):None,
+                'Starting HP: {}'.format(gamestate.players[0].starting_hp):None,
                 'Pets':None,
                 'Pet 1':None,
                 'Pet 2':None,
-                'Pet Name: {}'.format(pet1_name):None,
-                'Pet Name: {}'.format(pet2_name):None,
+                'Pet Name: {}'.format(gamestate.players[0].pet_name):None,
+                'Pet Name: {}'.format(gamestate.players[1].pet_name):None,
                 'Pet Type: ':None,
                 'Current HP: ':None,
                 r'^(\n\r\n)':None,
-                'Round 1 Started':None,
                 }
 
 # pexpect list, this is a list of keys from the reponses dictionary
@@ -84,7 +76,6 @@ game_init()
 
 def send_att():
         child.write("{}\n".format('1').encode())
-        while True:
-                print(child.readline())
+        print(child.readline())
 
 send_att()
---
+++ BattlePets_Running/BattlePets/player.py
@@ -0,0 +1,9 @@
+class Player:
+  def __init__(self, name, player_type, pet_name, pet_type, starting_hp):
+    self.name = name
+    self.player_type = player_type
+    self.pet_name = pet_name
+    self.pet_type = pet_type
+    self.starting_hp = starting_hp
+    
+    

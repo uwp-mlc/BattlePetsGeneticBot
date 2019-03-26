c40808aba8236550708601f2225b058d46ae5e30
Lukas J Peterson <petersonlu@uwplatt.edu>
2019-03-26 16:46:09 -0500

    Adding gameStateCreator and gameStateInstance

---
+++ BattlePets_Running/BattlePEts/gameStateInstance.py
@@ -0,0 +1,13 @@
+class gameStateInstance:
+	oppType: int
+	oppCd: int
+	oppLastAtt: int
+	oppDmg: double
+	oppHealth: double
+	selfType: int
+	selfCd: int
+	selfLastAtt: int
+	selfDmg: double
+	selfHealth: double 
+
+

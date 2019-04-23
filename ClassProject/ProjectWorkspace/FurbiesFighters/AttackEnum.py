c2acf54a6c047c21c1ca8e5b11e6a9f9522ffef0
garrettluskey <garrett.luskey@gmail.com>
2019-04-23 16:47:54 -0500

    Added reversal of fortune to enum

--- ClassProject/ProjectWorkspace/FurbiesFighters/AttackEnum.py
+++ ClassProject/ProjectWorkspace/FurbiesFighters/AttackEnum.py
@@ -1,7 +1,11 @@
 from enum import Enum
-class Attack(Enum)
+class Attack(Enum):
     ROCK_THROW = 1
     PAPER_CUT = 2
     SCISSOR_POKE = 3
     SHOOT_THE_MOON = 4
-    REVERSAL_OF_FORTUNE = 5
+    ROF_ROCK = 5
+    ROF_PAPER = 6
+    ROF_SCISSORS = 7
+    ROF_STM = 8
+    ROF_ROF = 9
